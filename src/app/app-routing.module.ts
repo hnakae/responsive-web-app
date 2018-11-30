4794bbef000d28c2bbe45f507060b81f3ce566ef
PlayInstinct <nakae27@gmail.com>
2018-11-30 14:03:18 -0800

    built the header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AllTasksComponent } from './all-tasks/all-tasks.component';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'all-tasks', component: AllTasksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, AllTasksComponent, AppComponent];
